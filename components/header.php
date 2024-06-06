450e59263a4d2d5da9222593d6afc58372d7c4a0
Chris Lamberth <cmattic@gmail.com>
2024-06-06 16:39:07 -0400

    Update header.php
    Unneeded code

--- components/header.php
+++ components/header.php
@@ -4,7 +4,5 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	<?php get_template_part('components/favicon'); ?>
 	<?php wp_head(); ?>
 </head>
